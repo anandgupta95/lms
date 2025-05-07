14a0e2bb8b68fc9fefc82042c11b1f42baab6d86
anand gupta <anandgupta965193@gmail.com>
2025-05-07 13:18:02 +0530

    email and username uniqueness check in profile updation

---
+++ src/main/java/com/lms/dto/teacher/request/UserRequestDTO.java
@@ -0,0 +1,29 @@
+package com.lms.dto.teacher.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class UserRequestDTO {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    // Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
